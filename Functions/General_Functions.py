188c295c6a629ad5f6bf8b8be91af327d1ef0547
Eugene <ystolberg@gmail.com>
2022-01-11 10:03:37 -0500

    Revert "Functions and working basics"
    This reverts commit 1ff25dc0

--- Functions/General_Functions.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def convtimestamp(UNIXstring):
-    timestamp = datetime.datetime.fromtimestamp(UNIXstring)
-    date = timestamp.strftime('%Y-%m-%d')
-    day = timestamp.strftime('%A')
-    timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-    return [date, day, timestamp]
